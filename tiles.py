d9450cca480a0993e313e71b2abfc7067f7c587a
Josey <joseyvanaarsen@gmail.com>
2024-02-10 22:31:10 +0100

    d

--- tiles.py
+++ tiles.py
@@ -10,11 +10,13 @@ class Tile(pygame.sprite.Sprite):
         image = pygame.transform.scale(image, (size, size))
         return image
 
-    def create_tile(self, pos, size, tile_type):
+    def create_tile(self, pos, tile_type, size):
         if tile_type == 'grass':
-            image_path = "D:/Animations/graphics/map/Grass.png"
+            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/Grass.png"
         elif tile_type == 'dirt':
-            image_path = "D:/Animations/graphics/map/Dirt.png"
+            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/Dirt.png"
+        elif tile_type == 'border':
+            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/transparant.png"
 
         tile_image = self.load_image(image_path, size)
         self.image = tile_image
