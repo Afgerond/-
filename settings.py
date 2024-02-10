51efc2f6daa421d8833a0a2cfa7a45d65cb93050
Josey <joseyvanaarsen@gmail.com>
2024-02-10 20:20:29 +0100

    aa

--- settings.py
+++ settings.py
@@ -1,15 +1,19 @@
 level_map = [
-'B                                                        GG  B',
-'B                                GG            GG            B',
-'B                           GG         GG                    B',
-'B          GG     GG                                         B',
-'B                       GG                                   B',
-'B                                                        GGGGG',
-'B P                                        GGG   G   G   DDDDD',
-'BGGGGGG                    GGG  G  GGG    GDDD   D   D   DDDDD',
-'BDDDDDD               GGGGGDDD  D  DDDGGGGDDDD   D   D   DDDDD',   
-'BDDDDDD GGGGGGGGGGGGG DDDDDDDD  D  DDDDDDDDDDD   D   D   DDDDD',
-'BDDDDDD DDDDDDDDDDDDD DDDDDDDD  D  DDDDDDDDDDD   D   D   DDDDD']
+'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+'                                                               ',
+'                                                               ',
+'                                                               ',
+'B                                                        GG   B',
+'B                                GG            GG             B',
+'B                           GG         GG                     B',
+'B          GG     GG                                          B',
+'B                       GG                                    B',
+'B                                                        GGGGGB',
+'B P                                        GGG   G   G   DDDDDB',
+'BGGGGGG                    GGG  G  GGG    GDDD   D   D   DDDDDB',
+'BDDDDDD               GGGGGDDD  D  DDDGGGGDDDD   D   D   DDDDDB',   
+'BDDDDDD GGGGGGGGGGGGG DDDDDDDD  D  DDDDDDDDDDD   D   D   DDDDDB',
+'BDDDDDD DDDDDDDDDDDDD DDDDDDDD  D  DDDDDDDDDDD   D   D   DDDDDB']
 
 tile_size = 64
 WIDTH = 1200
