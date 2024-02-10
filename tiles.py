ae959700203e000f90576a40258eb9df6535a4f1
Josey <joseyvanaarsen@gmail.com>
2024-02-10 21:03:47 +0100

    d

--- tiles.py
+++ tiles.py
@@ -5,13 +5,14 @@ from sprites import import_flag
 class Tile(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.animations = []  # Initial animations as an empty list
 
     def load_image(self, image_path, size):
         image = pygame.image.load(image_path)
         image = pygame.transform.scale(image, (size, size))
         return image
 
-    def create_tile(self, pos, size, tile_type):
+    def create_tile(self, pos, tile_type, size):
         if tile_type == 'grass':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/Grass.png"
         elif tile_type == 'dirt':
@@ -19,7 +20,13 @@ class Tile(pygame.sprite.Sprite):
         elif tile_type == 'border':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/transparant.png"
         elif tile_type == 'flag':
-            self.import_flag()  # Importeer de vlaganimatie
+            self.import_flag()  # Import the flag animation
+
+        self.size = size  # Store the size as an attribute
+
+        tile_image = self.load_image(image_path, self.size)  # Use the size parameter
+        self.image = tile_image
+        self.rect = self.image.get_rect(topleft=pos)
 
     def import_flag(self):
         full_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/flag/'
@@ -37,7 +44,7 @@ class Tile(pygame.sprite.Sprite):
 
         image = animation[int(self.index)]
 
-        tile_image = self.load_image(image, size)  # Gebruik het juiste image-pad
+        tile_image = self.load_image(image, self.size)  # Use the stored size attribute
         self.image = tile_image
         self.rect = self.image.get_rect(topleft=self.rect.topleft)
 
