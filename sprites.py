607baf31ae7824ffc10948169dc93d661bc2a289
Josey <joseyvanaarsen@gmail.com>
2024-02-10 21:16:09 +0100

    d

--- sprites.py
+++ sprites.py
@@ -1,5 +1,4 @@
 from os import walk
-import os
 import pygame
 from settings import WIDTH, HEIGHT
 
@@ -18,15 +17,13 @@ def import_folder(path):
             surface_list.append(image_surf)
 
         return surface_list
-
+    
 def flag(path):
     surface_list = []
 
-    for _, __, img_files in walk(path):
+    for _,__,img_files in walk(path):
         for image in img_files:
-            full_path = os.path.join(path, image)  # Gebruik os.path.join om het pad correct samen te stellen
-            image_surf = pygame.image.load(full_path).convert_alpha()
-            image_surf = pygame.transform.scale(image_surf, None)
-            surface_list.append(image_surf)
-
-    return surface_list
+            full_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/flag/' + image
+        image_surf = pygame.image.load(full_path).convert_alpha()
+        image_surf = pygame.transform.scale_by(image_surf, None)
+        surface_list.append(image_surf)
