2d81de1408779076c1948651e610b644f5162ea3
Josey <joseyvanaarsen@gmail.com>
2024-02-12 21:09:42 +0100

    d

--- coins.py
+++ coins.py
@@ -16,7 +16,7 @@ coin_silver_3 = pygame.image.load("C:/Users/josey/Privé/Programmeren/Portfolio/
 coin_silver_4 = pygame.image.load("C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/silver/04.png").convert_alpha()
 
 
-class Coin:
+class Coin(pygame.sprite.Sprite):
     def __init__(self, coin_images, animation_speed):
         self.coin_images = coin_images
         self.animation_speed = animation_speed
