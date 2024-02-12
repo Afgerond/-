0759f496b336b152a9d0b15e75850fc1dc749be0
Josey <joseyvanaarsen@gmail.com>
2024-02-12 17:40:10 +0100

    d

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ class Cloud:
         
 cloud_list = []
 
-for _ in range(4):
+for _ in range(random.randint(4, 7)):
     x = random.randint(0, WIDTH)
     y = random.randint(0, HEIGHT // 2)
     speed = random.uniform(0.1, 0.8)
