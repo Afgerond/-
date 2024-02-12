c6be0e885cb964fadf1fa25cd6b9f0fedf0d7ae3
Josey <joseyvanaarsen@gmail.com>
2024-02-12 16:29:05 +0100

    d

--- tiles.py
+++ tiles.py
@@ -1,13 +1,10 @@
 import pygame
+import random
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type):
         super().__init__()
         self.tile_type = tile_type
-        
-        self.size = None
-        self.image = None
-        self.rect = None
 
     def load_image(self, image_path, size):
         image = pygame.image.load(image_path)
@@ -15,43 +12,26 @@ class Tile(pygame.sprite.Sprite):
         return image
 
     def create_tile(self, pos, size, tile_type):
-        if self.tile_type == 'grass':
+        if tile_type == 'grass':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/Grass.png"
-        elif self.tile_type == 'dirt':
+        elif tile_type == 'dirt':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/Dirt.png"
-        elif self.tile_type == 'border':
+        elif tile_type == 'border':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/transparant.png"
-        elif self.tile_type == 'killingborder':
-            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/transparant.png"  
-        elif self.tile_type == 'wheel':
-            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/wheel/Ship Helm Turn 01.png"
-        elif self.tile_type == 'spikes':
+        elif tile_type == 'killingborder':
+            image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/transparant.png"            
+        elif tile_type == 'spikes':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/spikes/Spikes.png"
-        elif self.tile_type == 'flag':
+        elif tile_type == 'flag':
             image_path = "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/flag/1.png"
-        elif self.tile_type == 'coin':
-            image_paths = ["C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/gold/01.png",
-                           "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/gold/02.png",
-                           "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/gold/03.png",
-                           "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/gold/04.png"]
-            self.animation_frames = [pygame.image.load(image_path).convert_alpha() for image_path in image_paths]
-            self.frame_index = 0
-            self.animation_delay = 200  # Vertraging tussen frames in milliseconden
-            self.last_frame_time = pygame.time.get_ticks()
-            self.image = self.animation_frames[self.frame_index]
+        elif tile_type == 'coin':
+            image_path = random.choice(["C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/gold/01.png", "C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/silver/01.png"])
         else:
             raise ValueError("Ongeldig tegeltype")
 
-        if self.tile_type != 'coin':
-            self.image = pygame.image.load(image_path).convert_alpha()
+        tile_image = self.load_image(image_path, size)
+        self.image = tile_image
+        self.rect = self.image.get_rect(topleft=pos)
 
-        self.image = pygame.transform.scale(self.image, self.size)
-        self.rect = self.image.get_rect(topleft=self.pos)
-
-    def update(self):
-        if self.tile_type == 'coin':
-            current_time = pygame.time.get_ticks()
-            if current_time - self.last_frame_time > self.animation_delay:
-                self.frame_index = (self.frame_index + 1) % len(self.animation_frames)
-                self.image = self.animation_frames[self.frame_index]
-                self.last_frame_time = current_time
+    def update(self, x_shift):
+        self.rect.x += x_shift
