900e766f5d6d38f59ed253bd1f1c1d4af939606a
Josey <joseyvanaarsen@gmail.com>
2024-02-12 13:16:29 +0100

    d

--- sprites.py
+++ sprites.py
@@ -10,28 +10,26 @@ background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 health = pygame.image.load("C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/assets/heart.png")
 health = pygame.transform.scale(health, (40, 40))
-health_rect = health.get_rect(topleft=(36, 42))
+health_rect = health.get_rect(topleft = (36, 42))
 
 coin = pygame.image.load("C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/01.png")
 coin = pygame.transform.scale(coin, (40, 40))
-coin_rect = coin.get_rect(topleft=(36, 82))
+coin_rect = coin.get_rect(topleft = (36, 82))
 
-level_data = [...]  # Level data
-surface = pygame.display.set_mode((WIDTH, HEIGHT))
-level_inst = Level(level_data, surface)
-amount_coins = level_inst.coins
+amount_coins = 0
 
 coin_txt = pygame.font.SysFont("arial", 40).render(f"{amount_coins}", "white", True)
-coin_txt_r = coin_txt.get_rect(topleft=(90, 82))
+coin_txt_r = coin_txt.get_rect(topleft = (90, 82))
 
 def import_folder(path):
     surface_list = []
 
     for _,__,img_files in walk(path):
+
         for image in img_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
             image_surf = pygame.transform.scale_by(image_surf, 0.1)
             surface_list.append(image_surf)
 
-    return surface_list
+        return surface_list
