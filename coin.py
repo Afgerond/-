64cbbb9808cf9fd4e90d3f6c9d9251a8a36c3584
Josey <joseyvanaarsen@gmail.com>
2024-02-13 21:37:49 +0100

    d

--- coin.py
+++ coin.py
@@ -1,5 +1,6 @@
 import pygame
 from sprites import import_folder, import_coins
+import random
 
 
 class Coin(pygame.sprite.Sprite):
@@ -12,7 +13,7 @@ class Coin(pygame.sprite.Sprite):
         self.image = self.animations['gold'][self.index]
         self.rect = self.image.get_rect(topleft = pos)
 
-        self.status = 'gold'
+        self.status = random.choice(['gold', 'silver'])
 
     def imports(self):
         character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/coins/'
@@ -24,6 +25,45 @@ class Coin(pygame.sprite.Sprite):
 
     def animatie(self):
         animation = self.animations['gold']
+
+        self.index += self.animation_speed
+
+        if self.index >= len(animation):
+            self.index = 0
+
+        image = animation[int(self.index)]
+        self.image = image
+
+    def update(self, x_shift):
+        self.animatie()
+        self.rect.x += x_shift
+
+import pygame
+from sprites import import_folder, import_coins
+
+
+class Wheel(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.imports()
+        self.index = 0
+        self.animation_speed = 0.05
+
+        self.image = self.animations[self.status][self.index]
+        self.rect = self.image.get_rect(topleft = pos)
+
+        self.status = 'random'
+
+    def imports(self):
+        character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/wheel/'
+        self.animations = {'random': []}
+
+        for animation in self.animations.keys():
+            full_path = character_path
+            self.animations[animation] = import_coins(full_path)
+
+    def animatie(self):
+        animation = self.animations['random']
 
         self.index += self.animation_speed
 
