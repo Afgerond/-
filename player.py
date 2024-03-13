62170aec53c3b2760d49d2e5e51d35bba74bb43c
Josey <joseyvanaarsen@gmail.com>
2024-03-13 11:06:05 +0100

    d

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         # Player Movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
-        self.gravity = 0.8
+        self.gravity = 3
         self.jump_speed = -16
 
         # Jump 
