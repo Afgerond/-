79dc57b104b3985a33ee3b281d90ac979b941d0e
Josey <joseyvanaarsen@gmail.com>
2024-03-17 20:21:34 +0100

    d

--- main.py
+++ main.py
@@ -62,43 +62,46 @@ for _ in range(random.randint(4, 7)):
     cloud = Cloud(x, y, speed, size, image)
     cloud_list.append(cloud)
 
-run = True
-while run:
-    coin_txt = pygame.font.SysFont("behnschrift", 45).render(f"{level.coins}", "white", True)
-    coin_txt_r = coin_txt.get_rect(topleft = (90, coin_rect.y + 10))
+def game():
+    run = True
+    while run:
+        coin_txt = pygame.font.SysFont("behnschrift", 45).render(f"{level.coins}", "white", True)
+        coin_txt_r = coin_txt.get_rect(topleft = (90, coin_rect.y + 10))
 
-    time = pygame.font.SysFont("behnschrift", 45).render(f"{round((pygame.time.get_ticks() / 1000), 1)}", "white", True)
-    time_r = time.get_rect(topleft = (90, 150))
+        time = pygame.font.SysFont("behnschrift", 45).render(f"{round((pygame.time.get_ticks() / 1000), 1)}", "white", True)
+        time_r = time.get_rect(topleft = (90, 150))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-    if health_bar.hp <= 0:
-        run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+        if health_bar.hp <= 0:
+            run = False
 
-    for cloud in cloud_list:
-        cloud.move()
+        for cloud in cloud_list:
+            cloud.move()
 
-    screen.fill("black")
+        screen.fill("black")
+        
+        screen.blit(background, (0, 0))
+
+        for cloud in cloud_list:
+            cloud.draw(screen)
+
+        health_bar.draw(screen)
+        screen.blit(health, health_rect)
+        screen.blit(coin_txt, coin_txt_r)
+        screen.blit(coin, coin_rect)
+        screen.blit(clock_icon, clock_rect)
+        screen.blit(time, time_r)
+        
+        level.run()
     
-    screen.blit(background, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
 
-    for cloud in cloud_list:
-        cloud.draw(screen)
-
-    health_bar.draw(screen)
-    screen.blit(health, health_rect)
-    screen.blit(coin_txt, coin_txt_r)
-    screen.blit(coin, coin_rect)
-    screen.blit(clock_icon, clock_rect)
-    screen.blit(time, time_r)
-     
-    level.run()
- 
-    pygame.display.update()
-    clock.tick(FPS)
+game()
