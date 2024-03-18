cb861bf903111131648e8dc7d969ceac3c8bcaa5
Josey <joseyvanaarsen@gmail.com>
2024-03-18 20:34:00 +0100

    d

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ for _ in range(random.randint(4, 7)):
     size = random.randint(30, 80)
     image = random.choice([big_cloud_image, small_cloud1_image, small_cloud2_image, small_cloud3_image])
     if image == big_cloud_image:
-        cloud = Cloud(x, 390, speed, 250, image)
+        cloud = Cloud(x, 350, speed, 250, image)
     else:
         cloud = Cloud(x, y, speed, size, image)
     cloud_list.append(cloud)
