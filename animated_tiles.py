4cbb54c7786f5d1a149f8a4a6a96ab956e4d6cfe
Josey <joseyvanaarsen@gmail.com>
2024-03-20 14:20:01 +0100

    d

--- animated_tiles.py
+++ animated_tiles.py
@@ -131,3 +131,54 @@ class MovingPlatforms(pygame.sprite.Sprite):
             self.direction = -1
         elif self.direction == -1 and self.rect.x <= self.start - self.range:
             self.direction = 1
+
+class Enemies(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.imports()
+        self.index = 0
+        self.animation_speed = 0.15
+        
+        self.speed = 1.25
+        self.direction = 1
+        self.start = pos[0]
+        self.range = 50
+
+        self.type = random.choice(["crab", "pinky", "toothy"])
+
+        self.image = self.animations['run'][self.index]
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def imports(self):
+        if self.type == "crab":
+            character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/enemys/crab/'
+        elif self.type == "pinky":
+            character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/enemys/pinky/'
+        elif self.type == "toothy":
+            character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/enemys/toothy/'
+        self.animations = {'run': [], 'hit': [], 'dead': []}
+
+        for animation in self.animations.keys():
+            full_path = character_path
+            self.animations[animation] = import_wheel(full_path)  
+
+    def animatie(self):
+        animation = self.animations['run']
+
+        self.index += self.animation_speed
+
+        if self.index >= len(animation):
+            self.index = 0
+
+        image = animation[int(self.index)]
+        self.image = image
+
+    def update(self, x_shift):
+        self.animatie()
+        self.rect.x += self.direction * self.speed + x_shift
+        self.start += x_shift
+
+        if self.direction == 1 and self.rect.x >= self.start + self.range:
+            self.direction = -1
+        elif self.direction == -1 and self.rect.x <= self.start - self.range:
+            self.direction = 1
--- level.py
+++ level.py
@@ -3,54 +3,8 @@ from tiles import *
 from settings import tile_size, WIDTH
 from player import Player
 from health import *
-from animated_tiles import Coin, Wheel, Flag, MovingPlatforms
+from animated_tiles import Coin, Wheel, Flag, Enemies, MovingPlatforms
 from sprites import coin_collect, coin_decrease, import_wheel
-
-class Cannon(pygame.sprite.Sprite):
-    def __init__(self, pos):
-        super().__init__()
-        self.imports()
-        self.index = 0
-        self.animation_speed = 0.15
-
-        self.image = self.animations['idle'][self.index]
-        self.rect = self.image.get_rect(topleft = pos)
-
-        self.status = 'idle'
-
-    def imports(self):
-        character_path = 'C:/Users/josey/Privé/Programmeren/Portfolio/Platformer/Animations/graphics/map/enemys/cannon/idle'
-        self.animations = {'idle': []}
-
-        for animation in self.animations.keys():
-            full_path = character_path
-            self.animations[animation] = import_wheel(full_path)
-
-    def animatie(self):
-        animation = self.animations['idle']
-
-        self.index += self.animation_speed
-
-        if self.index >= len(animation):
-            self.index = 0
-
-        image = animation[int(self.index)]
-        self.image = image
-
-    def shoot(self):
-        player = self.player.sprite
-        player_x = player.rect.centerx
-        direction_x = player.direction.x
-
-        cannon_x = self.rect.centerx
-        distance = abs(player_x - cannon_x)
-
-        if distance <= 40:
-            pass
-
-    def update(self, x_shift):
-        self.animatie()
-        self.rect.x += x_shift
 
 class Level:
     def __init__(self, level_data, surface):
@@ -67,7 +21,7 @@ class Level:
         self.coin = pygame.sprite.Group()
         self.wheel = pygame.sprite.Group()
         self.flag = pygame.sprite.Group()
-        self.cannon = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
         self.diamond = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
         self.platform = pygame.sprite.Group()
@@ -114,8 +68,8 @@ class Level:
                     flag = Flag((x, y))
                     self.flag.add(flag)
                 elif cell == 'L':
-                    cannon = Cannon((x, y))
-                    self.cannon.add(cannon)
+                    enemy = Enemies((x, y))
+                    self.enemies.add(enemy)
                 elif cell == 'A':
                     platform = MovingPlatforms((x, y))
                     self.platform.add(platform)
@@ -282,8 +236,8 @@ class Level:
         self.flag.draw(self.display_surface)
 
         # Cannon
-        self.cannon.update(self.world_shift) # Snelheid waarmee de map een bepaalde kant op beweegt!
-        self.cannon.draw(self.display_surface)
+        self.enemies.update(self.world_shift) # Snelheid waarmee de map een bepaalde kant op beweegt!
+        self.enemies.draw(self.display_surface)
 
         # Platforms
         self.platform.update(self.world_shift) # Snelheid waarmee de map een bepaalde kant op beweegt!
