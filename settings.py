0a8f417ac87839af3b1fedd185f3b0dae0ae61bb
Josey <joseyvanaarsen@gmail.com>
2024-03-20 14:53:22 +0100

    d

--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
 level_map = [
-'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
-'    B                                                                B     ',
-'    B                                                          CS    B     ',
-'    B                                               SCS       GGG    B     ',
-'    B                             SC   CS    SS     GGG              B     ',
-'    B                   SC        GG   GG    GG                      B     ',
-'    B           AA      GG    CS                                     B     ',
-'    B                         GG                                S C FB     ',
-'    B                                               S   C   C   GGGGGGGGGGG',
-'    B 1  C SS                     W   C  S        GGG   G   G   DDDDDDDDDDD',
-'GGGGGGGGGGGGG               C  E GGG  G  GGG L   GDDD   D   D   DDDDDDDDDDD',
-'DDDDDDDDDDDDD SSS   L   SSS GGGGGDDD  D  DDDGGGGGDDDD   D   D   DDDDDDDDDDD',   
-'DDDDDDDDDDDDD GGGGGGGGGGGGG DDDDDDDD  D  DDDDDDDDDDDD   D   D   DDDDDDDDDDD',
-'DDDDDDDDDDDDDKDDDDDDDDDDDDDKDDDDDDDDKKDKKDDDDDDDDDDDDKKKDKKKDKKKDDDDDDDDDDD',
-'DDDDDDDDDDDDDKDDDDDDDDDDDDDKDDDDDDDDKKDKKDDDDDDDDDDDDKKKDKKKDKKKDDDDDDDDDDD']
+'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+'    B                                                                 B     ',
+'    B                                                           CS    B     ',
+'    B                                                SCS       GGG    B     ',
+'    B                             SC   CS    SS      GGG              B     ',
+'    B                   SC        GG   GG    GG                       B     ',
+'    B           AA      GG    CS                                      B     ',
+'    B                         GG                                 S C FB     ',
+'    B                                                S   C   C   GGGGGGGGGGG',
+'    B 1  C SS                     W   C  S         GGG   G   G   DDDDDDDDDDD',
+'GGGGGGGGGGGGG               C  E GGG  G  GGG  L   GDDD   D   D   DDDDDDDDDDD',
+'DDDDDDDDDDDDD SSS   L   SSS GGGGGDDD  D  DDDGGGGGGDDDD   D   D   DDDDDDDDDDD',   
+'DDDDDDDDDDDDD GGGGGGGGGGGGG DDDDDDDD  D  DDDDDDDDDDDDD   D   D   DDDDDDDDDDD',
+'DDDDDDDDDDDDDKDDDDDDDDDDDDDKDDDDDDDDKKDKKDDDDDDDDDDDDDKKKDKKKDKKKDDDDDDDDDDD',
+'DDDDDDDDDDDDDKDDDDDDDDDDDDDKDDDDDDDDKKDKKDDDDDDDDDDDDDKKKDKKKDKKKDDDDDDDDDDD']
 
 
 tile_size = 48
