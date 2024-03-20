7bfd3fba55bdcb84f5c410559d3ab3d99e866fbd
Josey <joseyvanaarsen@gmail.com>
2024-03-20 14:31:55 +0100

    d

--- sprites.py
+++ sprites.py
@@ -49,7 +49,7 @@ def import_enemies(path):
         for image in img_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
-            image_surf = pygame.transform.scale_by(image_surf, (2))
+            image_surf = pygame.transform.scale_by(image_surf, (1.875))
             surface_list.append(image_surf)
 
         return surface_list
